ef5a60777092e61d0c4c78140f0394bd863f34d6
dnhem27 <seoungdavid.nhem@gmail.com>
2021-07-06 06:29:25 -0400

    Finish lesson 42 exercise

--- web-forms/src/components/SignupForm.vue
+++ web-forms/src/components/SignupForm.vue
@@ -14,7 +14,7 @@
 
     <label>Skills (press alt + comma to add):</label>
     <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
-    <div v-for="skill in skills" :key="skill" class="pill">
+    <div v-for="skill in skills" :key="skill" class="pill" @click="deleteSkill(skill)">
       {{ skill }}
     </div>
 
@@ -52,6 +52,9 @@ export default {
         this.tempSkill = ''
       }
     },
+    deleteSkill(skill) {
+      this.skills.splice(this.skills.indexOf(skill), 1)
+    }
   }
 }
 </script>
@@ -89,5 +92,18 @@ export default {
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
+  }
+
+  .pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
   }
 </style>
